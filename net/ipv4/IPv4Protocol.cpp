d10bc103705f743896789f2bb3e6d77cec0ba86a
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-07-26 00:51:45 -0700

    InternetChecksum: do carry-out reduction at the end of the loop rather than each iteration

--- net/ipv4/IPv4Protocol.cpp
+++ net/ipv4/IPv4Protocol.cpp
@@ -63,9 +63,7 @@ uint16_t IPv4Protocol::InternetChecksum(uint8_t* data, uint16_t len, uint16_t in
 	uint32_t checksum = initial;
 	while(len >= 2)
 	{
-		//Add with carry
 		checksum += __builtin_bswap16(*data16);
-		checksum = (checksum >> 16) + (checksum & 0xffff);
 
 		data16 ++;
 		len -= 2;
@@ -73,10 +71,10 @@ uint16_t IPv4Protocol::InternetChecksum(uint8_t* data, uint16_t len, uint16_t in
 
 	//Add the last byte if needed
 	if(len & 1)
-	{
 		checksum += __builtin_bswap16(*reinterpret_cast<uint8_t*>(data16));
-		checksum = (checksum >> 16) + (checksum & 0xffff);
-	}
+
+	//Handle carry-out
+	checksum = (checksum >> 16) + (checksum & 0xffff);
 	return checksum;
 }
 
